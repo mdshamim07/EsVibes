63f407cd09cc65ddc22fff293fd49c1c750a69ba
codingshamim <rakibkhanshamim790@gmail.com>
2024-11-21 13:01:47 +0600

    first commit

--- app/(pages)/tshirt/[slug]/_components/ProductOrder.js
+++ app/(pages)/tshirt/[slug]/_components/ProductOrder.js
@@ -5,7 +5,7 @@ import BuyNowButton from "@/app/components/BuyNowButton";
 import Link from "next/link";
 import { useState } from "react";
 
-export default function ProductOrder({ sizes, productId }) {
+export default function ProductOrder({ stock, sizes, productId }) {
   const [activeSize, setActiveSize] = useState(sizes[0]);
   const [count, setCount] = useState(1);
   const increament = () => {
@@ -46,29 +46,35 @@ export default function ProductOrder({ sizes, productId }) {
         </button>
       </div>
       <div className="mt-4 flex items-center gap-3">
-        <Link
-          href={`/checkout?size=${activeSize}&quantity=${count}&product=${productId}`}
-          className="btn flex justify-between items-center gap-3 border"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={16}
-            height={16}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-shopping-bag"
-          >
-            <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
-            <path d="M3 6h18" />
-            <path d="M16 10a4 4 0 0 1-8 0" />
-          </svg>
-          <span>Buy now</span>
-        </Link>
-        <AddCart size={activeSize} quantity={count} productId={productId} />
+        {stock === 0 ? (
+          <button className="variable-btn nav-border text-red-600">Out Of Stock</button>
+        ) : (
+          <>
+            <Link
+              href={`/checkout?size=${activeSize}&quantity=${count}&product=${productId}`}
+              className="btn flex justify-between items-center gap-3 border"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={16}
+                height={16}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-shopping-bag"
+              >
+                <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
+                <path d="M3 6h18" />
+                <path d="M16 10a4 4 0 0 1-8 0" />
+              </svg>
+              <span>Buy now</span>
+            </Link>
+            <AddCart size={activeSize} quantity={count} productId={productId} />
+          </>
+        )}
       </div>
     </>
   );
